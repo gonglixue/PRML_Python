2f027e5fa48b96b97ac75b98f7b4909349818172
gonglixue <724351282@qq.com>
2017-09-21 18:56:44 +0800

    use mxnet to train a linear regression model

--- MXNET/LinearRegression.py
+++ MXNET/LinearRegression.py
@@ -20,6 +20,51 @@ def data_iter():
     # generate random indices
     idx = list(range(num_examples))
     random.shuffle(idx) # randomly sort
-    for i in range(0, num_examples, batch_size):
+    for i in range(0, num_examples, batch_size): #1000 examples and fetch 10 each time
         j = nd.array(idx[i: min(i+batch_size, num_examples)])
         yield nd.take(X, j), nd.take(y,j) # ?
+
+
+# for data, label in data_iter():
+#     print(data, label)
+#     break;
+
+w = nd.random_normal(shape= (num_inputs, 1))
+b = nd.zeros((1, )) # random is also ok
+params = [w, b]
+
+for param in params:
+    param.attach_grad()
+
+# define model
+def net(X):
+    return nd.dot(X, w) + b # return the prediction value
+
+# loss
+def square_loss(yhat, y):
+    return (yhat - y.reshape(yhat.shape)) ** 2
+
+# optimization
+def SGD(params, lr):
+    for param in params:
+        param[:] = param - lr * param.grad; # why param[:]
+
+
+# training
+epochs = 5  # scan 5 times for raw data
+learning_rate = 0.001
+for e in range(epochs):
+    total_loss = 0
+    for data, label in data_iter():
+        with ag.record():
+            output = net(data)
+            loss = square_loss(output, label)  # label is the true value in traing set
+        loss.backward()
+        SGD(params, learning_rate)
+
+        total_loss += nd.sum(loss).asscalar() # to float
+    print("Epoch %d, average loss: %f" % (e, total_loss/num_examples))
+
+print(true_b, b);
+print(true_w, w);
+

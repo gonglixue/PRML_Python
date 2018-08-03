12a26c334227cef4b9cf2ea2b8f7cec88263fb01
gonglixue <724351282@qq.com>
2018-08-03 20:23:00 +0800

    inference mode

--- TensorFlow/keras_official_tutorials/iris_flower.py
+++ TensorFlow/keras_official_tutorials/iris_flower.py
@@ -1,5 +1,5 @@
 import os
-
+import sys
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -20,7 +20,7 @@ class FlowerClassifier():
         model = keras.models.Model(inputs=inputs, outputs=predictions)
         return model
 
-    def train(self, learning_rate, momentum, batch_size, epochs, train_set_csv_path, val_set_csv_path, ckpt_dir='checkpoints'):
+    def train(self, learning_rate, momentum, batch_size, epochs, train_set_csv_path, ckpt_dir='checkpoints'):
         if not os.path.exists(ckpt_dir):
             os.mkdir(ckpt_dir)
 
@@ -29,25 +29,18 @@ class FlowerClassifier():
             target_dtype=np.int,
             features_dtype=np.float32
         )
-        val_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-            filename=val_set_csv_path,
-            target_dtype=np.int,
-            features_dtype=np.float32
-        )
 
         train_data = training_set.data
         train_target = training_set.target
-        test_data = val_set.data
-        test_target = val_set.target
 
         train_target = keras.utils.to_categorical(train_target, num_classes=3)
-        test_target = keras.utils.to_categorical(test_target, num_classes=3)
 
-        ckpt_file_path = os.path.join(ckpt_dir, "ckpt-improve-{epoch:02d}-{acc:.2f}.hdf5")
+
+        ckpt_file_path = os.path.join(ckpt_dir, "ckpt-improve-{epoch:02d}-{loss:.2f}.hdf5")
 
         callbacks = [
             keras.callbacks.TensorBoard(log_dir="iris_log", histogram_freq=0, write_grads=True, write_images=False),
-            keras.callbacks.ModelCheckpoint(ckpt_file_path, monitor='acc',
+            keras.callbacks.ModelCheckpoint(ckpt_file_path, monitor='loss',
                                             save_weights_only=True, save_best_only=True, verbose=1)  # val_loss, val_acc, loss
         ]
 
@@ -61,6 +54,24 @@ class FlowerClassifier():
         # score = self.keras_model.evaluate(test_data, test_target, batch_size=batch_size)
         # test_predict = self.keras_model.predict(test_data)
         # print(np.argmax(test_predict, axis=1))
+
+    def inference(self, test_set_csv_path, ckpt_path):
+        self.keras_model.load_weights(ckpt_path)
+        self.keras_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+            filename=test_set_csv_path,
+            target_dtype=np.int,
+            features_dtype=np.float32
+        )
+        test_data = test_set.data
+        test_target = test_set.target
+        test_target = keras.utils.to_categorical(test_target, num_classes=3)
+
+        scores = self.keras_model.evaluate(test_data, test_target, verbose=1)
+
+        print("%s: %.2f" % (self.keras_model.metrics_names[1], scores[1]))
+
 
 def test():
     training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
@@ -76,5 +87,9 @@ def test():
 
 if __name__ == '__main__':
     Classifier = FlowerClassifier()
-    Classifier.train(learning_rate=0.005, momentum=0.9, batch_size=32, epochs=50,
-                     train_set_csv_path='../../iris_training.csv', val_set_csv_path='../../iris_test.csv')
+
+    if sys.argv[1] == 'train':
+        Classifier.train(learning_rate=0.005, momentum=0.9, batch_size=32, epochs=50,
+                         train_set_csv_path='../../iris_training.csv')
+    elif sys.argv[1] == 'test':
+        Classifier.inference(test_set_csv_path='/home/gonglixue/PycharmProjects/PRML_Python/iris_test.csv', ckpt_path='/home/gonglixue/PycharmProjects/PRML_Python/TensorFlow/keras_official_tutorials/checkpoints/iris-ckpt-best.hdf5')
